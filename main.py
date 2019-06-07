6e4c73e21edcc7ba3477d69133def00a245098cc
hornig <chefkueche@hornshouse.com>
2019-06-07 20:54:41 +0200

    ads walking through all files in a folder and decoding their QRs
    adds walking through all files ina  folder and subfolders (recursive walk).
    
    decodes all the QR codes, when inside an image.
    
    just displays the basic data.
    cannot handle the exceptions yet.

--- main.py
+++ main.py
@@ -1,26 +1,65 @@
 import argparse
 import os
 import zxing
+from pathlib import Path
 
-def main(file):
-    if os.path.isdir(args.file) == False and os.path.isfile(args.file) == False:
-        print("neither file nor folder. ending programm.")
 
-    if os.path.isdir(args.file) == True:
-        print("loading in all files in folder:", file)
+def get_all_files(filename):
 
-    elif os.path.isfile(args.file) == True:
-        print("loading in this file:", file)
+    processing_files = []
+    for root, dirs, files in os.walk(filename):
+        for file in files:
+            filepath = os.path.join(root, file)
+            if os.path.isfile(filepath) == True:
+                print("file", filepath)
+                processing_files.append(filepath)
+
+    return processing_files
+
+def get_one_file(filename):
+    return [filename]
+
+
+def get_qr_codes(processing_files):
+    for file in processing_files:
+
+        # zxing doesn't like windows separator and other strange characters, so we just change it
+        file = Path(file)
+        file = file.absolute().as_uri()
 
         reader = zxing.BarCodeReader()
         barcode = reader.decode(file)
+        # does not handle exceptions yet, when the file is NOT an image
 
         if barcode is not None:
+            print("decoding", file)
             print(barcode.format)
             print(barcode.type)
             print(barcode.raw)
             print(barcode.parsed)
             print(barcode.points)
+            print()
+
+
+def main(filename):
+    if os.path.isdir(args.file) == False and os.path.isfile(args.file) == False:
+        print("neither file nor folder. ending programm.")
+
+
+    else:
+        if os.path.isdir(args.file) == True:
+            print("loading in all files in folder:", filename)
+
+            processing_files = get_all_files(filename)
+
+        elif os.path.isfile(args.file) == True:
+            print("loading in this file:", filename)
+
+            processing_files = get_one_file(filename)
+
+        get_qr_codes(processing_files)
+
+
 
 
 def getArgs():
