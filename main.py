b930a8ddddd8fd3ea8d53598f372ee67b4931c78
quanten <quantenprojects@web.de>
2019-08-10 14:06:11 +0200

    make interface of get_qr_codes more abstract
    only return necessary information which doesn't depend on the used libary
    also add possibilty for multiple qr codes in one image

--- main.py
+++ main.py
@@ -28,8 +28,16 @@ def get_qr_codes(file):
     barcode = reader.decode(file, True)
     # filename, try_harder = True
     # does not handle exceptions yet, when the file is NOT an image
+    print(barcode)
 
-    return barcode.format, barcode.type, barcode.raw, barcode.parsed, barcode.points
+    # TODO check if QR Code
+    if barcode is not None:
+        valid = barcode.format is not None and barcode.format == "QR_CODE"
+        content = barcode.parsed
+        point = barcode.points[1]
+        return [[valid, content, point]]
+    else:
+        return [[False,None,None]]
 
 
 def main(filename):
@@ -54,60 +62,62 @@ def main(filename):
         return
 
     for file in processing_files:
-        format, type, raw, parsed, points = get_qr_codes(file)
+        found_qr_codes = get_qr_codes(file)
+        for found_qr_code in found_qr_codes:
+            valid, parsed, point = found_qr_code
 
-        if format is not None:
-            print("qr found in", file, parsed)
+            if valid:
+                print("qr found in", file, parsed)
 
-            #do stuff now...
+                #do stuff now...
 
-            ## type indicator cases
-            if parsed.find("/n/") > -1:
-                print("type indicator: n/ = OSM Node ID with Basis 64")
+                ## type indicator cases
+                if parsed.find("/n/") > -1:
+                    print("type indicator: n/ = OSM Node ID with Basis 64")
 
-            if parsed.find("/w/") > -1:
-                print("type indicator: w/ = OSM Way ID with Basis 64")
+                if parsed.find("/w/") > -1:
+                    print("type indicator: w/ = OSM Way ID with Basis 64")
 
-            if parsed.find("/a/") > -1:
-                print("type indicator: a/ = OSM Area ID with Basis 64")
+                if parsed.find("/a/") > -1:
+                    print("type indicator: a/ = OSM Area ID with Basis 64")
 
-            if parsed.find("/r/") > -1:
-                print("type indicator: r/ = OSM Relation ID with Basis 64")
+                if parsed.find("/r/") > -1:
+                    print("type indicator: r/ = OSM Relation ID with Basis 64")
 
-            if parsed.find("/g/") > -1:
-                print("type indicator: g/ = OSM Shortlink quadtiles format")
+                if parsed.find("/g/") > -1:
+                    print("type indicator: g/ = OSM Shortlink quadtiles format")
 
-            my_gcp_id = parsed
-            # when the local id was found with a type indicator, it will be replaced.
-            # otherwise the parsed text will directly be used in the next "simple locally defined payload" method.
+                my_gcp_id = parsed
+                # when the local id was found with a type indicator, it will be replaced.
+                # otherwise the parsed text will directly be used in the next "simple locally defined payload" method.
 
-            if parsed.find("/l/") > -1:
-                print("type indicator: l/ = locally defined payload")
-                my_gcp_id = parsed.split("l/")[1]
-                print("gcp id", my_gcp_id, "in the local my_gcp_list.txt")
-
-
-            ## simple locally defined payloads
-            for item in gcp_list:
-                if my_gcp_id == item[0]:
-                    print("\t found a known gcp (", item[1] ,"/", item[2] ,"/", item[3] ,") from your list")
-                    location_utm = utm.from_latlon(float(item[1]), float(item[2]))
+                if parsed.find("/l/") > -1:
+                    print("type indicator: l/ = locally defined payload")
+                    my_gcp_id = parsed.split("l/")[1]
+                    print("gcp id", my_gcp_id, "in the local my_gcp_list.txt")
 
 
-                    # OpenDroneMap GCP
-                    ## header
-                    if odm_gcp_header == 0:
-                        # this is still to understood, why ODM just allows one utm zone and that is in the header!
-                        f.write("WGS84 UTM " + str(location_utm[2]) + str(location_utm[3]) + "\n")
-                        odm_gcp_header = 1
-
-                    ## line by line saving the coordinates
-                    f.write((str(location_utm[0]) + " " + str(location_utm[1]) + " "  + item[3] + " "
-                             + str(points[1][0]) + " "  + str(points[1][1]) + " "  + file.split(os.sep)[-1])  + "\n")
+                ## simple locally defined payloads
+                for item in gcp_list:
+                    if my_gcp_id == item[0]:
+                        print("\t found a known gcp (", item[1] ,"/", item[2] ,"/", item[3] ,") from your list")
+                        location_utm = utm.from_latlon(float(item[1]), float(item[2]))
 
 
-        else:
-            print("qr not found in", file)
+                        # OpenDroneMap GCP
+                        ## header
+                        if odm_gcp_header == 0:
+                            # this is still to understood, why ODM just allows one utm zone and that is in the header!
+                            f.write("WGS84 UTM " + str(location_utm[2]) + str(location_utm[3]) + "\n")
+                            odm_gcp_header = 1
+
+                        ## line by line saving the coordinates
+                        f.write((str(location_utm[0]) + " " + str(location_utm[1]) + " "  + item[3] + " "
+                                + str(point[0]) + " "  + str(point[1]) + " "  + file.split(os.sep)[-1])  + "\n")
+
+
+            else:
+                print("qr not found in", file)
 
 
     f.close()
