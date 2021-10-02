a7c2cf61799936c91c35aa799cdd8eb005dd1e26
Andre Herbst <moormaster@gmx.net>
2021-10-03 00:51:36 +0200

    call GCPURLParser after QRCode recognition

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 import os
+from typing import Callable
 
 from gcposm import utils
+from gcposm import model
+
+from gcposm.gcp_url_parser import GCPURLParser, GeoLocationResolver, GeoLocationDecoder
 
 import argument_parser
 import qr_code_extractor
@@ -17,8 +21,27 @@ def load_your_gcp_list(file):
 
     return gcp_list
 
+def init_gcp_url_parser(lookup: Callable[[str], model.GeoLocation]):
+    geo_location_resolver = GeoLocationResolver(lookup)
+    geo_location_decoder = GeoLocationDecoder()
+
+    return GCPURLParser(geo_location_resolver, geo_location_decoder)
 
 def main(filename):
+    gcp_lookup_table = load_your_gcp_list("my_gcp_list.txt")
+
+    def lookup_geo_location_in_lookup_table(id: str):
+        for entry in gcp_lookup_table:
+            [entry_id, latitude, longitude, altitude_in_meters] = entry
+            if id != entry_id:
+                continue
+
+            return model.GeoLocation(latitude, longitude, altitude_in_meters)
+
+        raise Exception("Id not found in lookup table: " + id)
+
+    gcp_url_parser = init_gcp_url_parser(lookup_geo_location_in_lookup_table)
+
     print("Hello, starting GCP-OSM...")
     print("")
 
@@ -37,7 +60,10 @@ def main(filename):
 
     for file in processing_files:
         found_qr_codes = qr_code_extractor.get_qr_data(file, debug_show_image=args.is_debug)
-        print(found_qr_codes)
+        print(str(found_qr_codes) + " -> ", end='')
+
+        [gcp_url, upper_left_position_marker_pixel_coordinates] = found_qr_codes
+        print(gcp_url_parser.parse(gcp_url))
 
     print("")
     print("GCP-OSM is finished, Good Bye!")
